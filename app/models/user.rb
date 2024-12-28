88f6b6939b6561c8eb916105d139c9aecf10bc53
terumitt <f4k0m6r0@icloud.com>
2024-12-28 16:04:04 +0900

    1対多になっていたので1対1に修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :scores, dependent: :destroy
+  has_one :scores, dependent: :destroy
 end
