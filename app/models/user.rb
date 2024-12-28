5127b00c3298fe520ec26aaf9317c30ce44245bc
terumitt <f4k0m6r0@icloud.com>
2024-12-28 17:17:32 +0900

    userモデルとmatchモデルと関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :scores, dependent: :destroy
+  has_one :user_matches, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_28_072039) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_28_075042) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -37,6 +37,15 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_28_072039) do
     t.index ["user_id"], name: "index_scores_on_user_id"
   end
 
+  create_table "user_matches", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "match_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["match_id"], name: "index_user_matches_on_match_id"
+    t.index ["user_id"], name: "index_user_matches_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -50,4 +59,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_28_072039) do
   end
 
   add_foreign_key "scores", "users"
+  add_foreign_key "user_matches", "matches"
+  add_foreign_key "user_matches", "users"
 end
