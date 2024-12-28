51b76eefe361a123171320b41f70e5e604b9e279
terumitt <f4k0m6r0@icloud.com>
2024-12-28 15:53:59 +0900

    db:migrateを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_24_141602) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_28_062013) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,14 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_24_141602) do
     t.integer "card_flag"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "scores", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "point"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_scores_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +40,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_24_141602) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "scores", "users"
 end
